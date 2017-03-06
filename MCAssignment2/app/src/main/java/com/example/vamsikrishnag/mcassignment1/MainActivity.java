4ce90d1094c2d5e9caa237cb2d178b4bfa4ce529
Dhananjayan Santhanakrishnan <sdjcse1@gmail.com>
2017-03-06 11:52:01 -0700

    finished upload

--- MCAssignment2/app/src/main/java/com/example/vamsikrishnag/mcassignment1/MainActivity.java
+++ MCAssignment2/app/src/main/java/com/example/vamsikrishnag/mcassignment1/MainActivity.java
@@ -1,20 +1,16 @@
 package com.example.vamsikrishnag.mcassignment1;
 
-import android.content.Context;
 import android.content.Intent;
-import android.database.Cursor;
-import android.support.v7.app.AppCompatActivity;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewDebug;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.Toast;
-import android.database.sqlite.SQLiteDatabase;
 
 public class MainActivity extends AppCompatActivity  {
 
